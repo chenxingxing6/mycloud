f3a85ef54614e94c213fff83116a3f98278cca2f
chenxingxing6 <1527072012@qq.com>
2019-01-01 21:41:00 +0800

    后台搭建完毕

--- mycloud-admin/src/main/java/com/example/MycloudAdminApplication.java
+++ mycloud-admin/src/main/java/com/example/MycloudAdminApplication.java
@@ -5,9 +5,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @MapperScan(basePackages = {"com.example.modules.*.dao"})
+@EnableSwagger2
+/*@EnableEurekaClient
+@EnableFeignClients
+@EnableTransactionManagement*/
 public class MycloudAdminApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
